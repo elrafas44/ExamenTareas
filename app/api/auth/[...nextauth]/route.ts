03d56b59dd204052597f57305e22652bfe39e89b
elrafas44 <rafitroxi@gmail.com>
2025-04-24 19:22:50 -0600

    cambio2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,41 +4,6 @@ import { authOptions } from "./authOptions";
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
-
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials.password) {
-          return null;
-        }
-
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-
-        if (!user) return null;
-
-        const isValid = await compare(credentials.password, user.password);
-        // Retornamos el objeto con la estructura que necesitamos
-        return {
-          id: user.id.toString(), // Convertimos a string
-          email: user.email,
-          name: user.email // Opcional: puedes añadir más campos
-        };
-      },
-    }),
-  ],
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, token }) {
       return {
         ...session,
         user: {
